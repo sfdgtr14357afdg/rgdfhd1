3153520bfd61e85f123ab4aff1b2dbfd78c60c5e
sfdgtr14357afdg <88231427+sfdgtr14357afdg@users.noreply.github.com>
2022-11-07 06:30:03 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # configs
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=7cf94aae-7d5b-4b68-b145-d158ab006eb4
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/test/test/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/test/test/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/sfdgtr14357afdg/rgdfhd1/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/sfdgtr14357afdg/rgdfhd1/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 
